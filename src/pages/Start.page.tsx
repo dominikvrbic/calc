817138849a2f1fe9af9e6026eaab5f94efada451
Dominik Vrbic <dominikvrbic01@gmail.com>
2021-04-19 21:23:19 +0200

    Initial commit

---
+++ src/pages/Start.page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+export const StartPage = (): JSX.Element => {
+  return (
+    <>
+      <Text as="h1">Start here</Text>
+    </>
+  );
+};
